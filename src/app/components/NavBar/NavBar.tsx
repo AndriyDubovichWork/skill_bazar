c5367dd5202daa1f4777612df8d2ff54a0284e49
Andriy Dubovich <andriydubovichwork@gmail.com>
2023-06-19 12:45:06 +0300

    basics

---
+++ src/app/components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './NavBar.module.scss';
+
+export default function NavBar() {
+  return (
+    <div className={classes.NavBar}>
+      <h2 className={classes.Logo}>Logo</h2>
+      <div>hello</div>
+    </div>
+  );
+}

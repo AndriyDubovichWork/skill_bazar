c1ab3e2e5433278138fb6995b543673d87914740
Andriy Dubovich <andriydubovichwork@gmail.com>
2023-06-19 21:33:04 +0300

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import classes from './Login.module.scss';
+import Input from '../components/Styled/Input/Input';
+import PasswordInput from '../components/Styled/PasswordInput/PasswordInput';
+import Button from '../components/Styled/Button/Button';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <form className={classes.Login}>
+      <h1 className={classes.Header}>register</h1>
+
+      <Input type='email' placeholder='email' />
+      <PasswordInput
+        placeholder='enter password'
+        showPassword={showPassword}
+        setShowPassword={setShowPassword}
+      />
+
+      <Button type={'button'} value='login' />
+    </form>
+  );
 }
